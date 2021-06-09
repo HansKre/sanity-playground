e3330e1f6c43cd92394428f02e274cfd2c16b11b
hans <hans.krebs@me.com>
2021-06-09 10:10:06 +0200

    Cleanup project: delete surrounding React project and get rid of example schemas

--- src/sanity-client.js
+++
@@ -1,8 +0,0 @@
-import sanityClient from '@sanity/client';
-
-export default sanityClient({
-    projectId: process.env.REACT_APP_SANITY_PROJECTID,
-    dataset: process.env.REACT_APP_SANITY_DATASET,
-    apiVersion: '2021-03-25',
-    useCdn: true
-})
